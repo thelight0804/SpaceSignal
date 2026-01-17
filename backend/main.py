72517c6fb842da8c672e7e0ab5c53c713eeeebeb
thelight0804 <thelight0804@gmail.com>
2026-01-17 11:55:36 +0900

    feat: Add CORS middleware and health check frontend #2

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from datetime import datetime
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
---
+++ frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    let health;
+
+    fetch("http://127.0.0.1:8000/health").then((response) => {
+        response.json().then((json) => {
+            health = json;
+        });
+    });
+</script>
+
+{#if health}
+    <div>
+        <h1>Status: {health.status}</h1>
+        <p>Version: {health.version}</p>
+        <p>Timestamp: {health.timestamp}</p>
+    </div>
+{:else}
+    <p>Loading...</p>
+{/if}
